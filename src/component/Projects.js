5cdcf28615ba76a44bb779fafe462a0a1a3dc462
vandu1979 <vandana_kothari@yahoo.com>
2023-02-02 11:14:16 -0700

    added more project link

--- src/component/Projects.js
+++ src/component/Projects.js
@@ -7,7 +7,7 @@ export default function Projects() {
    
     <section className="my-section">
         <h1 className='project'> Projects </h1> <br /> <br />
-        eslint-disable-next-line 
+     
         <a href="https://vandu1979.github.io/-matching-memory-game/" target='_blank'  rel="noreferrer">Memory Game</a> <br /> <br />
         <a href="https://vandu1979.github.io/SpaceShip-Battle/SpaceShip-Battle/" target='_blank'  rel="noreferrer">SpaceShip-Battle</a> <br /> <br />
         <a href="https://memory-client-xhdb.onrender.com/" target='_blank'  rel="noreferrer">Memory-Card MERN Full Stack App</a> <br /> <br />

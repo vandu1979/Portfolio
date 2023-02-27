4406712ef9618f5c5b976b04356a3f9fdb656a44
vandu1979 <vandana_kothari@yahoo.com>
2023-02-27 13:17:36 -0700

    link

--- src/component/Projects.js
+++ src/component/Projects.js
@@ -8,11 +8,13 @@ export default function Projects() {
     <section className="my-section">
         <h1 className='project'> Projects </h1> <br /> <br />
      
-        <a href="https://vandu1979.github.io/-matching-memory-game/" target='_blank'  rel="noreferrer">Memory Game</a> <br /> <br />
+       
         <a href="https://vandu1979.github.io/SpaceShip-Battle/SpaceShip-Battle/" target='_blank'  rel="noreferrer">SpaceShip-Battle</a> <br /> <br />
-        <a href="https://memory-client-xhdb.onrender.com/" target='_blank'  rel="noreferrer">Memory-Card MERN Full Stack App</a> <br /> <br />
-        <a href="https://mern-frontend-kxrn.onrender.com" target='_blank'  rel="noreferrer">Check-In-Register</a> <br /> <br />
+        <a href="https://vandu1979.github.io/-matching-memory-game/" target='_blank'  rel="noreferrer">Memory Game</a> <br /> <br />
         <a href="https://pokemon-app-7012.onrender.com/pokemon" target='_blank'  rel="noreferrer">Pokemon-App</a> <br /> <br />
+        <a href="https://memory-client-xhdb.onrender.com/" target='_blank'  rel="noreferrer">Mern Stack Memory-Card-App</a> <br /> <br />
+        <a href="https://mern-frontend-kxrn.onrender.com" target='_blank'  rel="noreferrer">Mern Stack Check-In-App</a> <br /> <br />
+       
         
        
         <Link to="/">Home</Link>
